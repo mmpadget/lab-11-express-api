633e7ea503086c016890ac93c904d7e39a5333c9
Michael Padget <mpadget@comcast.net>
2017-05-02 11:25:55 -0700

    Updated lib

--- lab-padget/model/music.js
+++ lab-padget/model/music.js
@@ -1,10 +1,12 @@
 'use strict';
-// eslint-disable-next-line
+// 1. constructor.
+
+// require debug module function and call it.
 const debug = require('debug')('http:music');
 const uuid = require('uuid/v4');
 
-// constructor
 module.exports = function(artist, album, song) {
+  debug('#music');
   if(!artist || !album) throw new Error('Invalid arguments');
   this.artist = artist;
   this.album = album;
